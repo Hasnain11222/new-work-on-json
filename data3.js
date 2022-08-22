a5a57b645340918f693e8ece3a93dcd526c6dd2b
Hasnain11222 <109806090+Hasnain11222@users.noreply.github.com>
2022-08-22 20:15:56 +0500

    Initial commit

---
+++ data3.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("login").innerHTML = aptech.login)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("id").innerHTML = aptech.id)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("node_id").innerHTML = aptech.node_id)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("avatar_url").innerHTML = aptech.avatar_url)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("gravatar_id").innerHTML = aptech.gravatar_id)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("url").innerHTML = aptech.url)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("html_url").innerHTML = aptech.html_url)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("followers_url").innerHTML = aptech.followers_url)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("following_url").innerHTML = aptech.following_url)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("gists_url").innerHTML = aptech.gists_url)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("starred_url").innerHTML = aptech.starred_url)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("subscriptions_url").innerHTML = aptech.subscriptions_url)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("organizations_url").innerHTML = aptech.organizations_url)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("repos_url").innerHTML = aptech.repos_url)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("events_url").innerHTML = aptech.events_url)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("received_events_url").innerHTML = aptech.received_events_url)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("type").innerHTML = aptech.type)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("site_admin").innerHTML = aptech.site_admin)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("name").innerHTML = aptech.name)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("company").innerHTML = aptech.company)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("blog").innerHTML = aptech.blog)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("location").innerHTML = aptech.location)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("email").innerHTML = aptech.email)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("hireable").innerHTML = aptech.hireable)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("bio").innerHTML = aptech.bio)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("twitter_username").innerHTML = aptech.twitter_username)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("public_repos").innerHTML = aptech.public_repos)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("public_gists").innerHTML = aptech.public_gists)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("followers").innerHTML = aptech.followers)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("following").innerHTML = aptech.following)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("created_at").innerHTML = aptech.created_at)
+fetch("https://api.github.com/users/Hasnain11222").then(res => res.json()).then(aptech => document.getElementById("updated_at").innerHTML = aptech.updated_at)
